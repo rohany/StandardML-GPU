4e775e2bfec8033ee7238553b36a9b854b81fc01
Brandon-Houghton <bhoughton@cmu.edu>
2017-05-05 09:36:20 -0400

    Added include files for float builtins closed TODO change theese to the float versions added is odd builtin

--- lib/funcptrs/builtin_filter_int.h
+++ lib/funcptrs/builtin_filter_int.h
@@ -4,10 +4,24 @@ __device__
 bool is_even_int(int x){
   return (x & 1) == 0;
 }
+
 __device__ filter_fun_int is_even_int_dev = is_even_int;
 extern "C"
 void* gen_is_even_int(){
   filter_fun_int local;
   cudaMemcpyFromSymbol(&local, is_even_int_dev, sizeof(filter_fun_int));
+  return (void*)local;
+}
+
+__device__
+bool is_odd_int(int x){
+  return (x & 1) == 1;
+}
+
+__device__ filter_fun_int is_odd_int_dev = is_odd_int;
+extern "C"
+void* gen_is_odd_int(){
+  filter_fun_int local;
+  cudaMemcpyFromSymbol(&local, is_odd_int_dev, sizeof(filter_fun_int));
   return (void*)local;
 }
